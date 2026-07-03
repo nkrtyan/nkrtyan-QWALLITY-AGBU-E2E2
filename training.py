255acc737dee223447201c2997e495e4e8c112d2
gayanekh <khachatryangayyaneee@gmail.com>
2026-07-03 15:17:23 +0400

    adding

---
+++ training.py
@@ -0,0 +1,13 @@
+print("Hello")
+print("My name is Gayane")
+print("Im learning python")
+print("Today I wrote my first program")
+name="Gayane"
+age=27
+city="Hrazdan"
+print(name)
+print(age)
+print(city)
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
