1ba39c23548025438c977318c01e8a21f9bad770
dianavetisyan <avetdian@gmail.com>
2026-07-03 10:17:12 +0400

    I have done text edits in Python code

--- lesson_21_workshop.py
+++ lesson_21_workshop.py
@@ -1,8 +1,8 @@
 print("It's my frst program!")
 print("Diana Avetisyan\n")
 print("I have a bachelor degree in Architecture and Design.")
-num1=20
-num2=80
-total_sum=num1+num2
+x=20
+y=80
+total_sum=x+y
 print(total_sum)
-# This is a multi-line comment.
+# This is just a comment.
