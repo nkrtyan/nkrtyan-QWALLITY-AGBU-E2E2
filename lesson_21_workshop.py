0eeb2a23870482e45da4de9d185cf3497e0eaf9f
hripsime.babayan <hripsime.babayan@synisys.com>
2026-07-03 14:01:29 +0400

    workshop_lesson_21 Hripsime

---
+++ lesson_21_workshop.py
@@ -0,0 +1,10 @@
+print ("lesson_21_workshop")
+print ("It's my first program!")
+print ("Hripsime Babayan")
+print ()
+print ("My background information:")
+a =10
+b = -5
+c = a + b
+print ("c = a + b =", c)
+# The End
