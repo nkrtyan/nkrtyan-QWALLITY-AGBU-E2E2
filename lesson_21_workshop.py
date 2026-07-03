f9aded3e241829785f041ac868af2ccf3f53b03e
Yelena Tadevosyan <elenatade@gmail.comL>
2026-07-03 09:50:21 +0400

    workshop-yelena

---
+++ lesson_21_workshop.py
@@ -0,0 +1,7 @@
+Print("It's my first program")
+Print("Yelena Tadevosyan")
+Print()
+Print("YSU, Faculty of Philosophy and Psychology,PhD Pholosophy")
+num1 = 15
+num2 = 27
+print(num1 + num2) # hope all is goodՃ
