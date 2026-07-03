804d77427ca6e06a08647981cfcfc1cfbd66c80c
Heriqnaz <heriqnaz.grigorian@gmail.com>
2026-07-03 11:16:20 +0400

    Create heriqnaz_grigoryan.py with initial content
    Add a simple Python program with print statements and a comment.

---
+++ heriqnaz_grigoryan.py
@@ -0,0 +1,8 @@
+print("It's my first program!")
+print("Heriknaz Grigoryan")
+print()
+print("Manual QA Engineer and Test Automation")
+a = 5
+b = 10
+print(a + b)
+# Սա իմ առաջին ավտոմատացված սկրիպտի մեկնաբանությունն է
