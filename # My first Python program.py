db31a1cbaeb47eae777c631a4a9fa0b9b25a8b2c
ArpAndreasyan <arpine.andreasyan93@gmail.com>
2026-07-04 01:37:21 +0400

    Change  done

---
+++ # My first Python program.py
@@ -0,0 +1,9 @@
+# My first Python program
+print("It's my first program!")
+print("Arpine Andreasyan")
+
+print()
+print("I studied at the Armenian State Pedagogical University.")
+num1 = 10
+num2 = 20
+print(num1 + num2)
