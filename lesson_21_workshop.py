925a3ed4f6f12afd31909019f2adebb4e1da6bc6
Anna Ayrapetyan <anna.ayrapetyan92@gmail.com>
2026-07-03 18:22:38 +0400

    add my changes

---
+++ lesson_21_workshop.py
@@ -0,0 +1,11 @@
+print("it's my first program")
+print("Anna Ayrapetyan\n")
+
+print("I studied Linguistics and now I teach English")
+number1 = 15
+number2 = 27
+print(number1 + number2)
+# this is my first python 
+print ("it's my first program")
+git config --global user.name "Anna Ayrapetyan"
+git config --global user.email "anna.ayrapetyan92@gmail.com"
