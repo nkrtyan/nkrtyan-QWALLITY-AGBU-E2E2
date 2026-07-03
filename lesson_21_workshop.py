2c9011c397f344b816f095ee4bb6a5290d1bd11a
MeriOnanyan <onanyanmeri04@gmail.com>
2026-07-03 09:48:52 +0400

    Workshop tasks

---
+++ lesson_21_workshop.py
@@ -0,0 +1,7 @@
+print("It's my first program", end="!\n")
+print("Meri Onanyan\n")
+print("I am a student studying at the National University of Architecture and Construction of Armenia.")
+x=5
+y=9
+sum=x+y #this line is calculating 5+9
+print(sum)
