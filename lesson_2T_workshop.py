5b9291fa8d7e6aeb6b1eb0ce36f6e546fcb99c45
gayahakobyan <gayanehakobyan1991@gmail.com>
2026-07-02 23:00:49 -0700

    complete lesson2

---
+++ lesson_2T_workshop.py
@@ -0,0 +1,9 @@
+print ("it's my first  program")
+print("Gayane Hakobyan")
+print ()
+print ("I am learning python")
+num1 =15
+num2 =10
+reslut = num1 + num2
+print ("result")
+# my first python program
