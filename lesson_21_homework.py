4621810fd901989cbe02d681adc0e0d5f634414e
Syuzanna Stepayan <stepanyansyuzi@gmail.com>
2026-07-03 13:23:11 +0400

    Homework

--- lesson_21_homework.py
+++ lesson_21_homework.py
@@ -1,4 +1,4 @@
 age = int(input("How old are you?\n"))
 decades = age // 10
 years = age % 10
-print(f"you are{dacades} decades and {years old."})
+print(f"you are {decades} decades and {yearss} old.")
