64d75956ca8249404b091fb7baf23763f255e3b2
elGevorgyan <elmira.gevorgyan@amaa.am>
2026-07-03 22:17:12 +0400

    add lesson 21 workshop: first Pyton program

---
+++ lesson_21_workshop.py
@@ -0,0 +1,11 @@
+print("It's my first program!")
+print("Elmira Gevorgyan")
+
+print("I am student learning Python")
+number_one = 30
+number_two = 31
+result = number_one + number_two
+print(result)
+
+
+#this is a comment
