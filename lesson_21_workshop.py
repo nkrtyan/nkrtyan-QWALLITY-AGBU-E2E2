406de4130913287d6455e76766a8c0c62144cf19
Heghine Grigoryan <heghine.grigoryan22@gmail.com>
2026-07-03 10:20:27 +0400

    Heghine's workshop

---
+++ lesson_21_workshop.py
@@ -0,0 +1,8 @@
+print("it's my first program")
+print("Heghine" , "Grigoryan") #my name and surname
+print("\n")
+print("I studied at YSU, Department of Mechanics and applied mathematics")
+x=5 
+y=5
+sum=x+y
+print(sum)
