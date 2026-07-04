82c0314f55b3f4df9d1ed502a96719540b0d5489
gayanekh <khachatryangayyaneee@gmail.com>
2026-07-04 23:37:10 +0400

    homework21Try number 2

--- homework.py
+++ homework.py
@@ -1,4 +1,9 @@
 age = int(input("Enter your age:"))
-decade = 27 // 10
-years = 27 % 10
+decade = age // 10
+years = age % 10
 print("You are", decade, "decades and", years, "year(s) old.")
+
+age = int(input("Enter your age:"))
+decade = age // 10
+years = age % 10
+print(f"You are {decade} decades and {years} year(s) old.")
