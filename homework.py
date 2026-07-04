529616bf80362005dd2d63ffa9a4f803785f4339
gayanekh <khachatryangayyaneee@gmail.com>
2026-07-04 23:10:21 +0400

    my homework for lesson22

---
+++ homework.py
@@ -0,0 +1,4 @@
+age = int(input("Enter your age:"))
+decade = 27 // 10
+years = 27 % 10
+print("You are", decade, "decades and", years, "year(s) old.")
---
+++ training3.py
@@ -0,0 +1,2 @@
+name, age, city = "Gayane", 27, "Hrazdan"
+print (f"{name} from {city}")
