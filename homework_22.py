310ab039de94ec764532bf80a23898d1e2a23e98
ekarapetyan <elelyak@gmail.comm>
2026-07-09 00:07:16 +0400

    Homework 22 file created

---
+++ homework_22.py
@@ -0,0 +1,22 @@
+'''Lesson_22: Homework->Implement List Operations
+Create one Python program that implements the following list operations:
+
+Find the maximum value in a list.
+Find the minimum value in a list.
+Calculate the sum of all elements in a list.
+Sort the list in ascending order.
+
+
+Requirements
+
+Use any list of numbers as input.
+Do not use Python's built-in max(), min(), sum(), sort(), or sorted() functions.
+Implement the logic yourself using the Python concepts you have learned so far.
+Print the original list and the result of each operation in the terminal.
+
+
+Submission
+
+Save your solution in one homework_22.py file.
+Commit and push your code to your Git repository.
+Create a Pull Request.'''
