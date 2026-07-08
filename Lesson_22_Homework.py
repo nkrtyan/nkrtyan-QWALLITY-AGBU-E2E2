10fa0904559f4e576d94a1daa5db9dfb3e3daef4
kristine <kristinetigra@gmail.com>
2026-07-08 23:08:05 +0400

    l

---
+++ Lesson_22_Homework.py
@@ -0,0 +1,46 @@
+my_list1 = [2, 5, 3, 9, 7, 12, 25, 38, 49] # Find the maximum value in a list.
+2 > 2 # no if max = 2
+5 > 2 # yes 5 = max
+3 > 5 # no 5 = max
+9 > 5 # yes 9 = max
+7 > 9 # no 9 = max
+12 > 9 # yes 12 = max
+25 > 12 # yes 25 = max
+38 > 25 # yes 38 = max
+49 > 38 # yes 49 = max
+maximum = 49
+
+my_list2 = [2, 5, 3, 9, 7, 12, 25, 38, 49] # Find the minimum value in a list.
+2 < 2 # no if min = 2
+5 < 2 # no 2 = min
+3 < 2 # no 2 = min
+9 < 2 # no 2 = min
+7 < 2 # no 2 = min
+12 < 2 # no 2 = min
+25 < 2 # no 2 = min
+38 < 2 # no 2 = min
+49 < 2 # no 2 = min
+minimum = 2
+
+my_list3 = [2, 5, 3, 9, 7, 12, 25, 38, 49] # Calculate the sum of all elements in a list.
+0 + 2 = 2
+2 + 5 = 7
+7 + 3 = 10
+10 + 9 = 19
+19 + 7 = 26
+26 + 12 = 38
+38 + 25 = 63
+63 + 38 = 101
+101 + 49 = 150
+sum = 150
+
+
+my_list4 = [2, 5, 3, 9, 7, 12, 25, 38, 49] # Sort the list in ascending order.
+2 > 5 # no
+5 > 3 # yes swap 5 and 3 (2, 3, 5, 9, 7, 12, 25, 38, 49)
+3 > 9 # no
+9 > 7 # yes swap 9 and 7 (2, 3, 5, 7, 9, 12, 25, 38, 49)
+7 > 12 # no
+12 > 25 # no
+25 > 38 # no
+38 > 49 # no
