11ff745dc4c3f1eb1919a508617e67ce040e7e0f
bkrtyan <nkrtyan@gmail.com>
2026-07-09 19:31:32 +0400

    Homework 22 has been reviewed.

--- lesson_22homework.py
+++ lesson_22homework.py
@@ -1,7 +1,5 @@
-
 #maximum
-
-list1 = [1, 5, 10, 14] 
+list1 = [1, 5, 10, 14] # TODO, if the list contains 1000 number , you will have 1000 if? :) Instead use for cycle
 if list1[0] < list1[1]:
     print (True)
 if list1[1] < list1[2]:
@@ -13,7 +11,7 @@ print (max(list1))
 
 #minimum
 
-list1 = [1, 5, 10, 14] 
+list1 = [1, 5, 10, 14]  # TODO, the same here
 if list1[0] < list1[1]:
     print (False)
 if list1[1] < list1[2]:
@@ -24,16 +22,23 @@ print (min(list1))
 
 #total 1 if we have no long numbers
 
-list1 = [1, 5, 10, 14] 
+list1 = [1, 5, 10, 14]  # TODO, the same here
 total = list1[0] + list1[1] + list1[2] + list1[3]
 print(total)
+# Nel, see below solution
+numbers = [11, 3, 35, 64, 57, 8, 79]
+total = 0
+for number in numbers:
+    total = total + number
+
+print(f"Sum of the list: {total}")
+
 
 #total 2
 
-list1 = [1, 5, 10, 14]
-total = 0
+list1 = [1, 5, 10, 14] 
 for item in list1:
     total = total + item 
 print(total)
 
-
+# TODO, task 4 is absent
