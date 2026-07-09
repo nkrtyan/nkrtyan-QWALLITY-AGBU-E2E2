b6608b8089b135654a69f48afbdcf34b6495e6e7
bkrtyan <nkrtyan@gmail.com>
2026-07-09 19:18:21 +0400

    Homework 22 has been reviewed.

--- homework_22.py
+++ homework_22.py
@@ -1,8 +1,6 @@
 
 # Homework 22 
-
 numbers = [44, 39, 11, 7, 75, 69, 104, 9, 55]
-
 print("Original list:", numbers)
 
 
@@ -12,7 +10,7 @@ for num in numbers:
     if num > max_value:
         max_value = num
 print("Maximum value:", max_value)
-
+# Nel, correct
 
 # 2. Find the minimum value
 min_value = numbers[0]
@@ -20,21 +18,22 @@ for num in numbers:
     if num < min_value:
         min_value = num
 print("Minimum value:", min_value)
-
+# Nel, correct
 
 # 3. Calculate the sum of all elements
 total = 0
 for num in numbers:
     total = total + num
 print("Sum of elements:", total)
-
+# Nel, correct
 
 # 4. Sort the list in ascending order
 size = len(numbers)
 for i in range(size - 1):
-    for j in range(size - 1 - i):
+    for j in range(size - 1 - i): # TODO,  this is good solution, hope you understand why to use -i-1
         if numbers[j] > numbers[j + 1]:
             temp = numbers[j]
             numbers[j] = numbers[j + 1]
             numbers[j + 1] = temp
 print("Sorted list:", numbers)
+# Nel, correct
