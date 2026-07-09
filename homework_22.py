40939e65ff375f6735f7e39d2fc7539d25eb2836
ekarapetyan <elelyak@gmail.comm>
2026-07-09 18:14:16 +0400

    homework 1-2-3 done, 4-not

--- homework_22.py
+++ homework_22.py
@@ -20,3 +20,28 @@ Submission
 Save your solution in one homework_22.py file.
 Commit and push your code to your Git repository.
 Create a Pull Request.'''
+
+my_list = [5, 2, 9, 1, 5, 6]
+print(my_list)
+
+#Find the maximum value in a list.
+maximum = my_list[0]
+for item in my_list:
+    if item > maximum:
+        maximum = item
+print("Maximum value:", maximum)
+
+#Find the minimum value in a list.
+minimum = my_list[0]
+for item in my_list:
+    if item < minimum:
+        minimum = item
+print("Minimum value:", minimum)
+
+#Calculate the sum of all elements in a list.
+summary = 0
+for item in my_list:
+    summary += item
+print("Sum of all elements:", summary)
+
+#Sort the list in ascending order. Couldn't use built-in sort() or sorted() functions
