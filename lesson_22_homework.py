96b989701d66bebd1a05f25db75d613b862654ba
bkrtyan <nkrtyan@gmail.com>
2026-07-09 19:23:29 +0400

    Homework 22 has been reviewed.

--- lesson_22_homework.py
+++ lesson_22_homework.py
@@ -5,24 +5,27 @@ for num in numbers:
     if num > maximum:
         maximum = num 
 print(maximum)
+
 minimum = numbers[0]
 for num in numbers:
     if num < minimum:
         minimum = num
 print(minimum)
+
 total = 0
 for num in numbers:
     total = total + num
 print(total)  
+
 sorted_list = numbers[:]
 n = len(sorted_list)
 for i in range(n):
-    for j in range(0, n - i - 1):
+    for j in range(0, n - i - 1): # TODO, I hope you understand why you use -i-1
         if sorted_list[j] > sorted_list[j + 1]:
             sorted_list[j], sorted_list[j + 1] = sorted_list[j + 1], sorted_list[j]
 print("Sorted list:", sorted_list)
 
-
+# TODO, code with 4 blocks are working correctly
 
 
 
