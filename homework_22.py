1f7e39f1af648916f2c22fdf6d3687fea87e4332
bkrtyan <nkrtyan@gmail.com>
2026-07-09 19:12:50 +0400

    Homework 22 has been reviewed.

--- homework_22.py
+++ homework_22.py
@@ -5,15 +5,17 @@ for number in my_list:
     if number>maximum:
         maximum=number
 print("maximum_value", maximum)
+# Nel, correct
+
 
 #Find the minimum value in a list.
-
 my_list=[45,15,56,78,14100,85,456,258,265,98,11256,56874]
 minimum=my_list[0]
 for number in my_list:
    if number<minimum:
       minimum=number
 print("minimum_value", minimum)
+# Nel, correct
 
 #Calculate the sum of all elements in a list.
 my_list=[45,15,56,78,14100,85,456,258,265,98,11256,56874]
@@ -21,15 +23,16 @@ summary=0
 for number in my_list:
    summary=summary+number
 print("sum", summary)
+# Nel, correct
 
 #Sort the list in ascending order.
-my_list=[45,15,56,78,14100,85,456,258,265,98,11256,56874]
+my_list=[45,15,56,78,14100,85,456,258,265,98,11256,1] # TODO, this will not work without loop, eveen need two nested loop
 if my_list[0] > my_list[1]:
     temp = my_list[0]
     my_list[0] = my_list[1]
     my_list[1] = temp
 
 print("sorted", my_list)
-
+# TODO, correct this tsak :)
 
 
