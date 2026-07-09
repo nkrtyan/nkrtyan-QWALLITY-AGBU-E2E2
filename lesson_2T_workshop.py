17e1c3de6b24c34283e42798ea4e474e91c3a679
gayahakobyan <gayanehakobyan1991@gmail.com>
2026-07-09 05:40:12 -0700

    homework_22.py

--- lesson_2T_workshop.py
+++
@@ -1,9 +0,0 @@
-print ("it's my first  program")
-print("Gayane Hakobyan")
-print ()
-print ("I am learning python")
-num1 =15
-num2 =10
-reslut = num1 + num2
-print ("result")
-# my first python program
