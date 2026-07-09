e21dee974cdbf953d5186ee0d86f61cebd6d5d6a
Lia-Amiraghyan <amiraghyanlia@gmail.com>
2026-07-09 15:12:46 +0400

    homework-22

---
+++ lesson_22_homework.py
@@ -0,0 +1,22 @@
+numbers = [15, 3, 28, 7, 10] 
+print (numbers)
+maximum = numbers[0]
+for num in numbers:
+    if num > maximum:
+        maximum = num 
+print(maximum)
+minimum = numbers[0]
+for num in numbers:
+    if num < minimum:
+        minimum = num
+print(minimum)
+total = 0
+for num in numbers:
+    total = total + num
+print(total)  
+ 
+
+
+
+
+ 
