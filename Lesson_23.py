be4b4a8ab8ceec663f2d70207f4f499f26378bd9
kristine <kristinetigra@gmail.com>
2026-07-10 23:30:06 +0400

    m

--- Lesson_23.py
+++ Lesson_23.py
@@ -1,4 +1,17 @@
+tuple1 = (2, 4, 6)
+tuple2 = (1, 3, 5)
+tupple3 = tuple1+tuple2
+print(tupple3[::-1])
+
+
 fruits_list = ["apple", "banana", "kiwi", "mango"]
 print(fruits_list[3])
 fruits_list.append("kiwi")
 print(list(set(fruits_list)))
+
+classmates_info = [
+    {"name": "Kristine", "surname": "Ghukasyan"},
+    {"name": "Bob", "surname": "Johnson"},
+    {"name": "Charlie", "surname": "Brown"}
+    ]
+print(classmates_info)
---
+++ Lesson_23_Homework.py
@@ -0,0 +1,2 @@
+"""#1 Write a Python program to remove duplicates from a list(write logic of the set, do not use set)"""
+my_numbers = [2, 5, 3, 9, 7, 5, 2, 12, 9, 25, 38, 25, 49]
