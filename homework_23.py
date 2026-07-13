7706918af493824b79e18f170a1355b5a8a0bf78
gayanekh <khachatryangayyaneee@gmail.com>
2026-07-13 14:59:49 +0400

    homework23

---
+++ homework_23.py
@@ -0,0 +1,117 @@
+#1 Write a Python program to remove duplicates from a list(write logic of the set, do not use set)
+numbers = [5, 2, 5, 8, 2, 1, 8]
+unique=[]
+for number in numbers:
+    if number not in unique:
+        unique.append(number)
+print(unique)
+
+
+#2 Write a Python program which print a specified list after removing the 0th, 4th and 5th elements.  
+my_list = [14, 40, 37, 7, 5, 6, 3]
+new_list = []
+for item in range(len(my_list)):
+    if item != 0 and item != 4 and item != 5:
+        new_list.append(my_list[item])
+print(new_list)
+
+
+#3 Write a Python program to get the difference between the two lists.
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+dif = []
+for item in list1:
+    if item not in list2:
+        dif.append(item)
+print(dif)
+
+
+#4 Write a Python program to convert a tuple to a dictionary.
+studentstuple=("Gayane",27), ("Alen", 28), ("Aram", 29)
+studentsdic = {}
+for item in studentstuple:
+    studentsdic[item[0]] = item [1]
+print(studentsdic)
+
+
+#5 Write a Python program to add an item in a tuple.
+#firstsolution
+cars = ("Mercedes", "Kia", "BMW", "Toyota")
+new_car = ("BYD")
+cars = cars + (new_car,)
+print(cars)
+#secondsolution
+cars = ("Mercedes", "Kia", "BMW", "Toyota")
+temp=list(cars)
+temp.append ("BYD")
+cars=tuple(temp)
+print(cars)
+
+
+#6 Write a Python program to add a key with value to a dictionary.
+
+book={
+    "book_name": "Harry Potter",
+    "years": 1997,
+    "author" : "J.K.Rowling"
+}
+print(book["author"])
+book["parts"] = 7
+print(book)
+
+
+#7 Write a Python program to get the maximum and minimum value in a dictionary().
+
+#first
+fam={
+   "Gayane": 1998,
+   "Anahit":2000,
+   "Albert":2005
+}
+values=list(fam.values())
+maximum = values[0]
+minimum = values [0]
+for number in values:
+    if number>maximum:
+        maximum=number
+    if number<minimum:
+        minimum=number
+print(maximum)
+print(minimum)
+#second
+fam={
+   "Gayane": 1998,
+   "Anahit":2000,
+   "Albert":2005
+}
+print(max(fam.values()))
+print(min(fam.values()))
+
+
+#8 Write a Python program to create a union of sets.
+#first
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+set3 = set1.union(set2)
+print(set3)
+#senond 
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+set3 = set1 | set2
+print(set3)
+
+#9 Student  Information
+student = {
+    "name": "Gayane",
+    "age": 27,
+    "address": "Hrazdan",
+    "education": "YSU",
+    "phone_numbers": ["+37494441207", "+37477100866"]
+}
+
+print(student)
+print(student["name"])
+print(student["phone_numbers"])
+
+student["email"] = "gayane.khachatran@gmail.com"
+print(student)
---
+++ workshop_23.py
@@ -0,0 +1,26 @@
+# tupl_1=(1,3,5,7)
+# tupl_2=(2,4,6,8)
+# tupl_3=tupl_1+tupl_2
+# print(tupl_3[::-1])
+
+
+# #2Create a list of your favorite fruits. 
+# # Print the third fruit in the list, add existing fruit to the list, then print list without duplicates.
+# my_list=["banana", "apple", "mango",]
+# print(my_list[2])
+# my_list.append("apple")
+# print(set(my_list))
+
+# print(list(set(my_list)))
+
+#Create a dictionary to store information about a person (e.g., name, surname). 
+# Print each person info.
+
+my_dic={
+    "name": "Gayane", 
+    "surname":"Khachatryan",
+    "education":"YSU"
+    }
+print(my_dic["education"])
+my_dic["age"]=27
+print(my_dic)
