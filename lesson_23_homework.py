30373ffb8c23b7ac86d52753d83b4b1c438703f1
Lia-Amiraghyan <amiraghyanlia@gmail.com>
2026-07-13 15:37:11 +0400

    done

---
+++ lesson_23_homework.py
@@ -0,0 +1,65 @@
+#1 Write a Python program to remove duplicates from a list(write logic of the set, do not use set)
+original_list = [1, 2, 2, 3, 4, 4, 5]
+unique_list = []
+for item in original_list:
+    if item not in unique_list:
+        unique_list.append(item)
+print(unique_list)
+
+#2 Write a Python program which print a specified list after removing the 0th, 4th and     5th elements.  
+my_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+my_list.remove(my_list[5])
+my_list.remove(my_list[4])
+my_list.remove(my_list[0])
+print(my_list)
+
+#3 Write a Python program to get the difference between the two lists
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+diff = []
+for item in list1:
+    if item not in list2:
+        diff.append(item)
+print(diff)
+
+#4 Write a Python program to convert a tuple to a dictionary.
+my_tuple = (("name", "Lia"), ("age", 20))
+my_dict = dict(my_tuple)
+print(my_dict)
+
+#5 Write a Python program to add an item in a tuple.
+my_tuple = ("my", "name", "lia")
+new_list = list(my_tuple)
+new_list.append("hello")
+my_tuple = new_list
+print(my_tuple)
+
+#6Write a Python program to add a key with value to a dictionary.
+my_dict = {"name": "Lia"}
+my_dict["age"] = 20
+print(my_dict)
+
+#7 Write a Python program to get the maximum and minimum value in a dictionary().
+data = {"a": 10, "b": 50, "c": 5}
+print("Max:", max(data.values()))
+print("Min:", min(data.values()))
+
+#8 Write a Python program to create a union of sets.
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union_set = set1.union(set2)
+print(union_set)
+
+#9 Student  Information.Write a Python program to create a dictionary with the following information about a student:
+dict9={
+    "name": "Lia",
+    "surname": "Amiraghyan",
+    "address": "Ijevan",
+    "education": "YSU",
+    "phone numbers": ["077737607", "098704569", "094258945"] 
+    }
+print(dict9)
+print(dict9["name"])
+print(dict9["phone numbers"])
+dict9["email"]="amiraghyanlia@gmail.com"
+print(dict9)
