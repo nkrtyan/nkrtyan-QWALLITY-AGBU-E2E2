b7d91d889bdc451e1787fb24c4b00cd9a797a352
dianavetisyan <avetdian@gmail.com>
2026-07-14 10:24:47 +0400

    Workshop_24

---
+++ Workshop_24.py
@@ -0,0 +1,10 @@
+number_list = [18, 2, 100, 44, 5]
+input_num = int(input("Enter a number: "))
+if input_num in number_list:
+    if input_num%2 == 0:
+        print(f"The number {input_num} is even.")
+    if input_num%2 != 0:
+        print(f"The number {input_num} is odd.")
+    print(f"The number {input_num} exists in the list.")
+else:
+    print(f"The number {input_num} is not in the list.")
