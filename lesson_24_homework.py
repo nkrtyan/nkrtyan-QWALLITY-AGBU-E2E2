99cb8589f55cc20965181d560728edefe0b53eef
Meri Onanyan <meri.onanyan@volo.global>
2026-07-15 16:30:53 +0400

    Homework 24

---
+++ lesson_24_homework.py
@@ -0,0 +1,35 @@
+#Write a Python program to get the Fibonacci series between 0 to 50
+sum = 0
+first_num = 0
+second_num = 1
+print(first_num)
+while sum <=50:
+    print(second_num)
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+print()
+
+#Write a Python program that accepts a string and calculates the number of digits and letters. 
+my_text = "Python 3.13"
+char_count = 0
+int_count = 0
+for char in "Python 3.13":
+    if char.isalpha():
+        char_count +=1
+    elif char.isdigit():
+        int_count +=1
+
+print(f"Digits count is: {int_count}")
+print(f"letters count is: {char_count}", "\n")
+
+
+#Write a Python program to print alphabet pattern 'L'
+i = 0
+while i<5:
+    print("*")
+    i+=1
+
+for item in range(5):
+    print("*", end=" ")
+
