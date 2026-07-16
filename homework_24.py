acf3fcd3cdb6d89776c31aef9741ab266b08d43f
bkrtyan <nkrtyan@gmail.com>
2026-07-16 19:36:12 +0400

    Homework 24 has been reviewed.

--- homework_24.py
+++ homework_24.py
@@ -7,14 +7,14 @@
 
 print()
 # 1.1
-# first_number = 0
-# second_number = 1
+first_number = 0
+second_number = 1
 
-# while first_number <= 50:
-#     print(f"{first_number}", end=" ")
-#     next_number = first_number + second_number
-#     first_number = second_number
-#     second_number = next_number
+while first_number <= 50:
+    print(f"{first_number}", end=" ")
+    next_number = first_number + second_number
+    first_number = second_number
+    second_number = next_number
 
 # 1.2
 fib_numbers = [0, 1] 
@@ -25,6 +25,7 @@ while fib_numbers[-1] <= 50:
 fib_numbers.pop()
 print(f"{fib_numbers}")
 
+# Nel, both versions are correct
 
 # 2. Write a Python program that accepts a string and calculates the number of digits and letters.  
 # Sample Data: Python 3.13
@@ -34,18 +35,19 @@ print(f"{fib_numbers}")
 
 print()
 # 2.1
-# my_string = "Python 3.13"
-# letters_count = 0
-# digits_count = 0
+my_string = "Python 3.13"
+letters_count = 0
+digits_count = 0
 
-# for char in my_string:
-#     if char.isalpha():
-#         letters_count += 1
-#     elif char.isdigit():
-#         digits_count += 1
+for char in my_string:
+    if char.isalpha():
+        letters_count += 1
+    elif char.isdigit():
+        digits_count += 1
 
-# print(f"Letters: {letters_count}")
-# print(f"Digits: {digits_count}")
+print(f"Letters: {letters_count}")
+print(f"Digits: {digits_count}")
+# Nel, correct
 
 # 2.2
 # text = input("Enter a string: ")
@@ -55,14 +57,14 @@ letters_count = 0
 digits_count = 0
 
 for char in text:
-    if (char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
+    if (char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'): 
         letters_count += 1
     elif char >= '0' and char <= '9':
         digits_count += 1
 
 print(f"Letters: {letters_count}")
 print(f"Digits: {digits_count}")
-
+# Nel, correct, but i prefer the firts version
 
 # 2. Write a Python program to print alphabet pattern 'L'  
 # Expected Output:
@@ -83,9 +85,10 @@ for i in range(line_count):
         print("*" * 5)
 
 # 3.2
-# line_count = [1, 2, 3, 4, 5, 6, 7]
-# for i in line_count[:6]:
-#     if line_count[i] != 6:
-#         print("*")
-#     else:
-#         print("*" * 5)
+line_count = [1, 2, 3, 4, 5, 6, 7]
+for i in line_count[:6]:
+    if line_count[i] != 6:
+        print("*")
+    else:
+        print("*" * 5)
+#Nel, both versions are correct
