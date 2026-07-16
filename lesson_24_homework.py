82833bc3001cc409011d00df65f42f0c34a145e5
bkrtyan <nkrtyan@gmail.com>
2026-07-16 19:34:23 +0400

    Homework 24 has been reviewed.

--- lesson_24_homework.py
+++ lesson_24_homework.py
@@ -5,8 +5,11 @@ while(b<50):
     a, b = b, a+b
     my_list.append(a)
 print(my_list)
+# TODO, correct,. but we dont need to see list in terminal, instead just items
+
 # 3
 for i in range(7):
     print("*")
-if(i==6):
-    print("*****")
+if(i==6): # TODO, this if should be inside for cycle
+    print("*****") # TODO, use 6*"*"
+ 
