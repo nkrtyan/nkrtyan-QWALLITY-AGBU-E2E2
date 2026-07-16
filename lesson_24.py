b0d92cd1175de610c386c7225360d8c470cd8267
bkrtyan <nkrtyan@gmail.com>
2026-07-16 19:22:43 +0400

    Homework 24 has been reviewed.

--- lesson_24.py
+++ lesson_24.py
@@ -2,29 +2,29 @@
 a = 0
 b = 1
 
-print("Ряд Фибоначчи:")
+print("Ряд Фибоначчи:") # TODO, type in lattin
 while a <= 50:
     print(a, end=" ")
 
-    a, b = b, a + b
+    a, b = b, a + b # TODO, this print should be as a part f the block
 
  
-    s = "Python 3.13"
+s = "Python 3.13"
 letters = 0
 digits = 0
 for symbol in s:
     if symbol.isalpha():
-        letters += 1  
-        
+        letters += 1         
     elif symbol.isdigit():
         digits += 1   
         
 print ("\nLetters", letters)
-
 print ("Digits", digits)
+# Nel, correct
 
 l = 0
 while l <= 6:
     print("\n*")
     l = l + 1
 print("*****")
+# TODO, no need extra line after each *
