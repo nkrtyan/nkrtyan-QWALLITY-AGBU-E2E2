6e374be4200cd49eb3f93bf65cf7d5b1be56a86d
dianavetisyan <avetdian@gmail.com>
2026-07-16 16:01:07 +0400

    Homeworks_24

---
+++ Homework_24.py
@@ -0,0 +1,32 @@
+max = 50
+for i in range(max):
+    if i > 1:
+        i = fib1 + fib2
+        fib1 = fib2
+        fib2 = i
+    else:
+        i = i
+        fib1 = 0
+        fib2 = 1
+    if i < max:
+        print(i, end = " ")
+print()
+
+my_text = "Python 3.13"
+letters = 0
+digits = 0
+
+for char in my_text:
+    if char.isalpha():
+        letters += 1
+    elif char.isdigit():
+        digits += 1
+
+print("Letters:", letters)
+print("Digits:", digits)
+
+height = 7
+for i in range(height - 1):
+    print("*")
+print("*****")
+
