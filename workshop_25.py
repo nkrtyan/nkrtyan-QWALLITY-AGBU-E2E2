8cbd55047357211b8c866b612c401f980cfe0d85
gayahakobyan <gayanehakobyan1991@gmail.com>
2026-07-16 22:44:14 -0700

    workshop_25.py

---
+++ workshop_25.py
@@ -0,0 +1,10 @@
+# number=int(input("input a number:"))
+# for item in range(1, 11):
+#     print(number,"*", item, "=", number * item)
+
+for item in range(1,10):
+    for j in range (item):
+        print(item, end="")
+    print()
+
+
