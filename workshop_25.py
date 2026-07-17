f549c89e7b9030b27b15c1166fcf11e9b50b259b
gayahakobyan <gayanehakobyan1991@gmail.com>
2026-07-16 22:49:49 -0700

    fail change

--- workshop_25.py
+++ workshop_25.py
@@ -2,9 +2,11 @@
 # for item in range(1, 11):
 #     print(number,"*", item, "=", number * item)
 
-for item in range(1,10):
-    for j in range (item):
-        print(item, end="")
-    print()
+# for item in range(1,10):
+#     for j in range (item):
+#         print(item, end="")
+#     print()
 
-
+number=int(input("input a number:"))
+for item in range(1, 11):
+   print(f"{number}*{item}={number*item}")
