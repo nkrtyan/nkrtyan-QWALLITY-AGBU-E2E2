13eaadad3d2505afc542fe87606c209f886cde89
siravetisyan <siranushav@gmail.com>
2026-07-20 02:11:14 +0400

    add person package

---
+++ person/utils.py
@@ -0,0 +1,11 @@
+from person import data
+
+def get_full_name(name, surname):
+    return name, surname
+
+def print_person():
+    person_data = data.my_dict
+    name, surname = get_full_name(person_data['name'], person_data['surname'])
+    print(f"{name} {surname}")
+
+    
