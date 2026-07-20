6f3de37f289a6f56b3312a2b2d19690f10015205
nkrtyan <nelly_krtyan@mail.ru>
2026-07-20 22:20:47 +0400

    Homework 25 has been reviewed.

--- Project/main.py
+++ Project/main.py
@@ -1,3 +1,5 @@
 from person.utils import print_person
-if __name__ == "__main__":
+if __name__ == "__main__": # TODO, I hope you read what this function do
     print_person()
+
+# Nel, code is fully correct
--- Project/person/utils.py
+++ Project/person/utils.py
@@ -1,6 +1,10 @@
 from person.data import person_data
+
+
 def get_full_name(name, surname):
     return f"{name} {surname}"
+
+
 def print_person():
     my_name = person_data["name"]
     my_surname = person_data["surname"]
