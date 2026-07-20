68d0b36c0807794ec9a9d9427a9b682b409c6df1
nkrtyan <nelly_krtyan@mail.ru>
2026-07-20 22:26:49 +0400

    Homework 25 has been reviewed.

--- Project/person/utils.py
+++ Project/person/utils.py
@@ -1,6 +1,10 @@
 from person.data import person_data
+
+
 def get_full_name(name, surname):
     return f"{name} {surname}"
+
+
 def print_person():
     name = person_data["name"]
     surname = person_data["surname"]
