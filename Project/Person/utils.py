66a0f2728cdfe81d03c70672fb421489b872db61
nkrtyan <nelly_krtyan@mail.ru>
2026-07-20 22:13:37 +0400

    Homework 25 has been reviewed.

--- Project/Person/utils.py
+++ Project/Person/utils.py
@@ -1,6 +1,10 @@
 from .data import person
+
+# TODO, keep two lines between funtions
 def get_full_name (name, surname):
     return  name + " " + surname
+
+
 def print_person():
     full_name = get_full_name(person["name"], person["surname"])
     print(full_name)
