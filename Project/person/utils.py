ee364e5ed42361c9b28c023eacfd749d6b9d6783
dianavetisyan <avetdian@gmail.com>
2026-07-20 11:59:35 +0400

    Homework_25

---
+++ Project/person/utils.py
@@ -0,0 +1,8 @@
+from person.data import person_data
+def get_full_name(name, surname):
+    return f"{name} {surname}"
+def print_person():
+    my_name = person_data["name"]
+    my_surname = person_data["surname"]
+    full_name = get_full_name(my_name, my_surname)
+    print(f"Full Name: {full_name}")
