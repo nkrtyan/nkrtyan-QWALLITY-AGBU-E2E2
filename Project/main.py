ae0ec613e30889fdd2d14ee6495d4437d34eec0d
bkrtyan <nkrtyan@gmail.com>
2026-07-20 19:25:22 +0400

    Homework 25 has been reviewed.

--- Project/main.py
+++ Project/main.py
@@ -1,5 +1,6 @@
 from person import utils
 # from person import data
 utils.print_person()
+# Nel, code mainly is correct, just check TODOs
 
 
--- Project/person/data.py
+++ Project/person/data.py
@@ -1,3 +1,4 @@
 dict1={}
 dict1["name"]="Elya"
 dict1["surname"]="Karapetyan"
+# TODO, change to dictonary, which has two items, name and surname with their values
