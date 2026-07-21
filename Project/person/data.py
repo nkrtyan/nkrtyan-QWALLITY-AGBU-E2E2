73736223d0ecaec80f34be7449a88f2b20469d35
ekarapetyan <elelyak@gmail.comm>
2026-07-21 09:12:08 +0400

    TO DO is done

--- Project/person/data.py
+++ Project/person/data.py
@@ -1,4 +1,5 @@
-dict1={}
-dict1["name"]="Elya"
-dict1["surname"]="Karapetyan"
+dict1={
+    "name": "Elya",
+    "surname": "Karapetyan"}
+
 # TODO, change to dictonary, which has two items, name and surname with their values
---
+++ lesson25.py
@@ -0,0 +1,35 @@
+# def greeting(name):
+#     print ("Hello", name)
+
+# input_name=input("Enter your name\n")
+# greeting(input_name)
+# greeting("Armen")
+
+# def print_me( name, age):
+#     "this is your age and name"
+#     print("Name:", name )
+#     print("Age:", age)
+#     return
+# print_me(age=33, name="Elya")
+
+# def display(*name, **address):
+#     for items in name:
+#         print(items)
+#     for items in address.items:
+#         print(items)
+# display("Elya", "Anna", "Mari", Elya="Yerevan", Anna="NY", Mari="CA")
+
+# def square(x):
+#     return x*x
+# print(square(4))
+
+# def square(x):
+#     print(x*x)
+# # print(square(4))
+
+# square(4)
+
+
+def addition(a, b):
+    return a+b
+print(addition(20, 25))
