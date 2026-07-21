706c3d19ba731de3a1d3319ea50535e31fbb4115
ekarapetyan <elelyak@gmail.comm>
2026-07-21 17:34:30 +0400

    Library created

--- lesson25.py
+++ lesson25.py
@@ -33,3 +33,42 @@
 def addition(a, b):
     return a+b
 print(addition(20, 25))
+
+book = {
+        "Python Crash Course": {
+        "author": "Eric Matthes",
+        "pages": 544,
+        "year": 2022
+    },
+
+    "Automate the Boring Stuff with Python": {
+        "author": "Al Sweigart",
+        "pages": 592,
+        "year": 2019
+    }
+}
+
+from book_data import book
+
+def display_books():
+    if not book:
+        print("Library is empty") 
+    else:
+        print(book)
+
+display_books()
+
+
+# def get_book(title):
+#     if title in book.keys():
+#         print(book[title])
+#     else:
+#         print("no book")
+# get_book(" Crash Course")
+
+
+# def add_book(title,author,page,year):
+#     book{}
+
+
+# def delete_book(title)
