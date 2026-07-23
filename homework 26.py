3ca0079423cdb28a2f4a1a87fc3279b6d99e7e91
bkrtyan <nkrtyan@gmail.com>
2026-07-23 18:31:09 +0400

    Homework  26 has been reviewed.

--- homework 26.py
+++ homework 26.py
@@ -5,10 +5,11 @@ with open("myfile.txt", "a+") as file:
     file.seek(0)
     lines = file.readlines()
 
-for line in lines:
+for line in lines: # TODO, this for loop should be inside with open block. 
     print(line.strip())
+# TODO, make this logic as an function and call it. Give file name as an argument 
 
-  #Task 2
+#Task 2
 def create_files():
     for i in range(ord('A'), ord('Z') + 1):
         letter = chr(i)
@@ -17,3 +18,4 @@ def create_files():
         print(f"File created: {filename}")
 
 create_files()
+# Nel, this function is correct
