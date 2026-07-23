ca65cbde1d86197bb3c27e5db2c5722f7e6708d4
ekarapetyan <elelyak@gmail.comm>
2026-07-23 19:21:22 +0400

    TODO-es are done

--- homework_26.py
+++ homework_26.py
@@ -4,14 +4,27 @@
 # Read data from the file and print in the Terminal. 
 
 
-f=open("test.file.txt", "a+")
-added_text=f.write("\nHello\nit’s my first file handling!")
-print(added_text)
-f.close()
-f1=open("test.file.txt", "r")
-changed_file=f1.read()
-print(changed_file)
+def create_file(filename):
+    with open(filename, "a+") as f:
+        added_text=f.write("Hello\nit’s my first file handling!")
+        print(added_text)
+
+    with open(filename, "r") as f1:
+        changed_file=f1.read()
+        print(changed_file)
+
+create_file("test.file.txt")
+
+# TODO, code is correct, but put the logic inside function and give filename as an argument
 
 # 2. Write a Python function which generates 26 text files named A.txt, B.txt, and so on up to Z.txt.
 
+# TODO, finish when have time
 
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for item in alphabet:
+    with open(item + ".txt", "w") as file:
+        pass
+    print(item+".txt")
+    
