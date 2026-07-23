b24ef522946b405070ac0729432316e9c6f47a39
bkrtyan <nkrtyan@gmail.com>
2026-07-23 18:10:56 +0400

    Homework  26 has been reviewed.

--- Lesson_26_Homework/Homework.py
+++ Lesson_26_Homework/Homework.py
@@ -2,7 +2,10 @@ f = open("my_text.txt", "a+") #1
 my_list = ["Hello\n", "it's my first handling!\n"]
 for i in my_list:
     f.write(i)
-
 f.seek(0)
 print(f.read())
 f.close()
+
+# TODO, make this logic as a function and call it, pass file name as an argument to the function
+# TODO, the second task is absent
+# TODO, do not commit files created by the code
--- Lesson_26_Homework/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def create_files():
-    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    for letter in letters:
-        file = open(letter + ".txt", "w")
-        file.close()
-
-create_files()
