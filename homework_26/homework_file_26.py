8c65eabcf40d2b44179b772f75592d36e5fe8a09
ytadevosyan <elenatade@gmail.com>
2026-07-23 11:25:30 +0400

    homework26

--- homework_26/homework_file_26.py
+++ homework_26/homework_file_26.py
@@ -6,7 +6,21 @@ Read data from the file and print in the Terminal.
 
 Write a Python function which generates 26 text files named A.txt, B.txt, and so on up to Z.txt.
 """
-f = open("test_file","a")
+f = open("test_file.txt","a")
 f.write("Hello\n")
 f.write("It's my first file handling!\n")
 f.close()
+
+f = open("test_file.txt", "r")
+data = f.read()
+print(data)
+f.close()
+
+
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for letter in letters:
+    filename = "homework_26/" + letter + ".txt"
+    f = open(filename, "w")
+    f.close()
