769532ab8afcd560f693e71092332ee708872dea
bkrtyan <nkrtyan@gmail.com>
2026-07-23 18:23:44 +0400

    Homework  26 has been reviewed.

--- homework_26.py
+++ homework_26.py
@@ -4,32 +4,27 @@
 # Read data from the file and print in the Terminal. 
 
 file = open("homework.txt", "a")
-
 file.write("Hello\n")
 file.write("it’s my first file handling!\n")
-
 file.close()
-
+# TODO, no need close the file, than open it again, use seek(0) operator
 
 file = open("homework.txt", "r")
-
 data = file.read()
-
 print(data)
-
 file.close()
-
+# TODO, no need extra lines between code
+# TODO, make this logic as an function and call it. Give the file name as an argument 
 
 
 # Write a Python function which generates 26 text files named A.txt, B.txt, and so on up to Z.txt.
 
 
-import string
+import string # TODO, import should be in the beggining of the file
 
 def creatingfiles():
     for letter in string.ascii_uppercase:
         file = open(f"{letter}.txt", "w")
         file.close()
-
-
 creatingfiles()
+# Nel, thsi part is correct
