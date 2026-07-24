c63689cd3bb4d317d39d2f0c099dc06ee49efb10
nkrtyan <nelly_krtyan@mail.ru>
2026-07-24 09:27:07 +0400

    aaa

--- Homework 26.py
+++ Homework 26.py
@@ -1,13 +1,16 @@
-f = open("newest.txt", "w+")
-f.write("Hello\nit is my first file handling!\n")
-f.seek(0)
-content = f.read()
-print(content)
+# f = open("newest.txt", "w+")
+# f.write("Hello\nit is my first file handling!\n")
+# f.seek(0)
+# content = f.read()
+# print(content)
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for letter in alphabet:
-    file_name= letter + ".txt"
-    open(file_name, "w")
+# alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# for letter in alphabet:
+#     file_name= letter + ".txt"
+#     open(file_name, "w")
 
 # TODO, both tasks should be moved under functions. File name should be given as an argument to the function
 # Nel, code part is correct
+
+with open("a.txt","w+") as file:
+    pass
