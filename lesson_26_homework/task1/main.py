160e4f9ea33ac46acc3e935afcb03593a64fa2f1
Meri Onanyan <meri.onanyan@volo.global>
2026-07-24 10:41:56 +0400

    updated homework

--- lesson_26_homework/task1/main.py
+++ lesson_26_homework/task1/main.py
@@ -1,12 +1,14 @@
-with open('lesson_26_homework/task1/test.txt', 'a+') as file:
-    my_list = ["Hello\n", "It's my first file handling!"]
+def open_file():
+    with open('lesson_26_homework/task1/test.txt', 'a+') as file:
+        my_list = ["Hello\n", "It's my first file handling!"]
 
-    for i in my_list:
-        file.writelines(i)
+        for i in my_list:
+            file.writelines(i)
 
-    file.seek(0)
-    file_read = file.read()
+        file.seek(0)
+        file_read = file.read()
 
-print(file_read)
+    print(file_read)
+open_file()
 # TODO, no need to commit test.txt file
-# TODO, put this functional inside function and call it
+# FIX, put this functional inside function and call it
