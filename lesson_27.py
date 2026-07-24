7919e1f358b25caf7cfe848deb452cc39bbe10f1
Meri Onanyan <meri.onanyan@volo.global>
2026-07-24 11:04:00 +0400

    Lesson 27

---
+++ lesson_27.py
@@ -0,0 +1,24 @@
+import os
+
+def creating_folder(folder_name, file_name,text):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+    os.chdir(folder_name)
+    with open(os.path.abspath(file_name), "w") as file:
+        file.write(text)
+
+    os.remove(file_name)
+    os.chdir("..")
+
+    user_input = input("Do you want remove the folder Y/N ")
+    if user_input == "Y":
+        os.rmdir(folder_name)
+
+if __name__ == "__main__":
+
+    folder_name = "New_folder"
+    file_name = "New_file"
+    text = "Workshop information"
+
+    creating_folder(folder_name, file_name, text)
