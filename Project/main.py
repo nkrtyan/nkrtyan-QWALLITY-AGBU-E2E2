939c46b79af46babc1e6f7d46165128d139927c4
Heghine Grigoryan <heghine.grigoryan22@gmail.com>
2026-08-13 17:28:10 +0400

    Ready for commiting

--- Project/main.py
+++
@@ -1,5 +0,0 @@
-from person.utils import print_person
- 
- 
-if __name__ == "__main__":
-    print_person()
---
+++ workshop28.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+import shutil
+import logging
+
+data = {
+    "Name": ["Anna", "David", "Emma", "Mark", "Sophia"],
+    "Subject": ["Python", "Python", "Python", "Python", "Python"],
+    "Score": [85, 92, 78, 88, 95],
+}
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="app.log",
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+def create_directory(directory_name):
+    if not os.path.exists(directory_name):
+        os.mkdir(directory_name)
+        logging.info(f"Directory '{directory_name}' created.")
+    else:
+        logging.info(f"Directory '{directory_name}' already exists.")
+
+def create_results_file(directory_name, file_name):
+    df= pd.DataFrame(data)
+    file_path = os.path.join(directory_name, file_name)
+    df.to_excel(file_path, index=False)
+    logging.info(f"Results data saved to '{file_path}'.")
+
+def analyze_results(directory_name, file_name):
+    file_path = os.path.join(directory_name, file_name)
+    df = pd.read_excel(file_path)
+    names = df["Name"]
+    scores = df["Score"]
+    num_students = len(names)
+    highest_score = scores.max()
+    lowest_score = scores.min()
+    logging.info(f"Number of examined students: {num_students}")
+    
+# analyze results function is not finished yet
+def cleanup(directory_name):
+    answer = input(f"Do you want to remove the directory '{directory_name}'? (y/n): ").strip().lower()
+    if answer == "y":
+        if os.path.exists(directory_name):
+            shutil.rmtree(directory_name)
+            logging.info(f"Directory '{directory_name}' removed.")
+        else:
+            logging.info(f"Directory '{directory_name}' does not exist, nothing to remove.")
+    else:
+        logging.info(f"Directory '{directory_name}' was kept (user chose not to remove it).")
+
+if __name__ == "__main__":
+    directory_name = "ExaminationResults"
+    file_name = "exam_results.xlsx"
+ 
+    setup_logging()
+    create_directory(directory_name)
+    create_results_file(directory_name, file_name)
+    analyze_results(directory_name, file_name)
+    cleanup(directory_name)
