9747f298dfab216e69e435087573a12543e04d83
Heghine Grigoryan <heghine.grigoryan22@gmail.com>
2026-08-13 17:26:36 +0400

    ready for commiting

---
+++ Project/person/data.py
@@ -0,0 +1,4 @@
+person = {
+    "name": "Heghine",
+    "surname": "Grigoryan"
+}
---
+++ Project/person/utils.py
@@ -0,0 +1,11 @@
+from person.data import person
+ 
+ 
+def get_full_name(name, surname):
+    return f"{name} {surname}"
+ 
+ 
+def print_person():
+    full_name = get_full_name(person["name"], person["surname"])
+    print(full_name)
+ 
