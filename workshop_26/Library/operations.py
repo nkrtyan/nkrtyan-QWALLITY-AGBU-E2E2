670ae2f245d6d75eb0bcb7a7b0261dd770fe9571
gayahakobyan <gayanehakobyan1991@gmail.com>
2026-08-13 00:45:15 -0700

    examination result

--- workshop_26/Library/operations.py
+++
@@ -1,8 +0,0 @@
-from Library.data import books
-def display_books(): 
-    for title, info in books.items():
-        print(f"{title}:{info}")
-display_books()
-
-def get_books("title":"info")
-        
