f7b53f5df14dbbdd77c27974ec949f876c237763
bkrtyan <nkrtyan@gmail.com>
2026-08-17 12:28:59 +0400

    Homework 29 has been reviewed.

--- homework_29.py
+++ homework_29.py
@@ -3,6 +3,7 @@ sports = {
     "price": [40000, 50000],
     "sessions": [16, 10]
 }
+# TODO, make dict like each sport as separate pair, like name: tennis, price: 40000. session: 16
 
 class Sport:
     def __init__(self, name, price, sessions):
@@ -14,8 +15,9 @@ class Sport:
         return self.price / self.sessions
 
 
-swimming_obj = Sport("Swimming", 40000, 16)
+swimming_obj = Sport("Swimming", 40000, 16) # TODO, do not hardcode data during object creation, you should get data from dict
 tennis_obj = Sport("Tennis", 50000, 10)
 
 print(f"{swimming_obj.name} - {swimming_obj.session_price()} AMD per session")
 print(f"{tennis_obj.name} - {tennis_obj.session_price()} AMD per session")
+# Nel, OOP is kept, but dict and data passing are incorrect
