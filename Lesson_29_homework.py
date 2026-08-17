02e98af4d3cb3a2e59474a0e39ef8783e5088a23
bkrtyan <nkrtyan@gmail.com>
2026-08-17 14:46:47 +0400

    Homework 29 has been reviewed.

--- Lesson_29_homework.py
+++ Lesson_29_homework.py
@@ -1,7 +1,14 @@
 #Option 1
+# TODO, keep this format
 courses = {
-    "swimming": {"price": 40000, "month": 16},
-    "tennis": {"price": 50000, "month": 10}
+    "swimming": {
+        "price": 40000, 
+        "month": 16
+        },
+    "tennis": {
+        "price": 50000, 
+        "month": 10
+        }
 }
 
 
@@ -15,8 +22,8 @@ class Sport:
         return self.price / self.session
 
 
-swimming_obj = Sport("swimming", 40000, 16)
-tennis_obj = Sport("tennis", 50000, 10)
+swimming_obj = Sport("swimming", 40000, 16) # TODO,  do not hard code data, you should get it from dict
+tennis_obj = Sport("tennis", 50000, 10) # TODO, the same here
 
 
 print(f"Swimming - {swimming_obj.show_calculates()} AMD per session")
@@ -47,3 +54,5 @@ tennis_obj = Sport("tennis", 50000, 10)
 
 print(f"Swimming - {swimming_obj.per_session} AMD per session")
 print(f"Tennis - {tennis_obj.per_session} AMD per session")
+
+# Nel,OOP is kept, but need to correct passing data ffrom dict
