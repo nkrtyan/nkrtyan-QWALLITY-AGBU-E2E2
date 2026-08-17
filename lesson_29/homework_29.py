45c263fe0b4ebe7dcbe720557ddeec12ad2bca84
bkrtyan <nkrtyan@gmail.com>
2026-08-17 12:15:59 +0400

    Homework 29 has been reviewed.

--- lesson_29/homework_29.py
+++ lesson_29/homework_29.py
@@ -5,6 +5,7 @@ data= {
     "price": [40000, 50000],
     "number_sessions": [16, 10]
 }
+# TODO, keep dict like sport_1 and data (dict value, like sport_name:Swimming, price:40000, number_of_session:16)
 
 class Sport():
     def __init__(self, name, price, number_sessions):
@@ -23,6 +24,7 @@ tennis_obj=Sport(data['name'][1], data["price"][1], data["number_sessions"][1])
 
 swimming_obj.show()
 tennis_obj.show()
+# Nel,  OOP is kept, code is working correctly, just would like to see another dict
 
 """Create a Python program to calculate the price of one training session for different sports.
 Use the following sports:
