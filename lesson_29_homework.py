b57434e3131d6bf4bbc91680b3bf98a84796be47
bkrtyan <nkrtyan@gmail.com>
2026-08-17 14:36:59 +0400

    Homework 29 has been reviewed.

--- lesson_29_homework.py
+++ lesson_29_homework.py
@@ -16,8 +16,10 @@ class Sport():
         print(f'{self.name} - {session_price} AMD per session')
 
 
-swimming_obj = Sport(data['name'][0], data['price'][0], data['number'][0])
+swimming_obj = Sport(data['name'][0], data['price'][0], data['number'][0]) # TODO, no need hardcode the index, instead keep each sport like seperate dict inside the dict
 tennis_obj = Sport(data['name'][1], data['price'][1], data['number'][1])
 
 swimming_obj.calculate()
 tennis_obj.calculate()
+
+# Nel, OOP is correct, correct dict and call
