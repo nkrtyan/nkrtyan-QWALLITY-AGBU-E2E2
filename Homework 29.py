d5cf51fe2550f82eb4bfef79ae8190b0e2da3c5d
bkrtyan <nkrtyan@gmail.com>
2026-08-17 14:38:45 +0400

    Homework 29 has been reviewed.

--- Homework 29.py
+++ Homework 29.py
@@ -3,6 +3,7 @@ sports = {
     "price": [40000, 50000],
     "sessions": [16, 10]
 }
+# TODO, keep dict like each sport type as a seperate dict inside one sports, no need keep list
 
 class Sport:
     def __init__(self, name, price, sessions):
@@ -14,8 +15,10 @@ class Sport:
         return self.price / self.sessions
 
 
-swimming_obj = Sport("Swimming", 40000, 16)
+swimming_obj = Sport("Swimming", 40000, 16) # TODO, do not hard code data, pass from dict
 tennis_obj = Sport("Tennis", 50000, 10)
 
 print(f"{swimming_obj.name} - {swimming_obj.session_price()} AMD per session")
 print(f"{tennis_obj.name} - {tennis_obj.session_price()} AMD per session")
+
+# Nel, OOP is kept, but dict part need correctness
