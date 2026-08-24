01ce51635931ffe80cd19fa41c374ed0464a30a5
ytadevosyan <elenatade@gmail.com>
2026-08-24 11:40:44 +0400

    added readme

--- homework_29.py
+++ homework_29.py
@@ -3,7 +3,9 @@ sport_types = {
     "Swimming": {"price": 40000, "sessions": 16},
     "Tennis": {"price": 50000, "sessions": 10}
 }
-#TODO, keep kson like two seperate sports, and in line 22 pass swimming like firts dict key, tennis like second key, no need hardcode swimming and tennis
+
+sport_names = list(sport_types.keys())
+#FIX, keep kson like two seperate sports, and in line 22 pass swimming like firts dict key, tennis like second key, no need hardcode swimming and tennis
 
 # Create a Sport class
 # Each sport should have a name, price, and number of sessions
@@ -19,8 +21,9 @@ class Sport:
 
 
 # Create objects for swimming_obj and tennis_obj
-swimming_obj = Sport("Swimming", sport_types["Swimming"]["price"], sport_types["Swimming"]["sessions"])
-tennis_obj = Sport("Tennis", sport_types["Tennis"]["price"], sport_types["Tennis"]["sessions"])
+swimming_obj = Sport(sport_names[0], sport_types[sport_names[0]]["price"], sport_types[sport_names[0]]["sessions"])
+tennis_obj = Sport(sport_names[1], sport_types[sport_names[1]]["price"], sport_types[sport_names[1]]["sessions"])
+
 
 # Print the session price for each sport
 print(f"{swimming_obj.name} - {swimming_obj.session_price()} AMD per session")
