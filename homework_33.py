9a5a23f1273ce26213ef0d658af1a42e69d5b1b5
Syuzanna Stepayan <stepanyansyuzi@gmail.com>
2026-08-30 23:14:58 +0400

    Done

---
+++ homework_33.py
@@ -0,0 +1,28 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test(driver):
+    driver.get("https://python.org")
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    
+    search = driver.find_element(By.ID, "id-search-field")
+    sent = driver.find_element(By.ID, "submit")
+    
+    search.send_keys("BlaBla")
+    sent.click()
+    
+    time.sleep(5)
+    
+    information = driver.find_element(By.CLASS_NAME, "list-recent-events")
+    
+    if "No results found." in information.text:
+        print("Everything is ok")
+    else:
+        print("Something went wrong")
+        
+    driver.quit()
+
+test(webdriver.Chrome())
+test(webdriver.Firefox())
