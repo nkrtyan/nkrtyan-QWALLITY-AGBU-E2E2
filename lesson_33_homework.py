9474d923c3e195f075469f64d69594f3f57c9ab5
Meri Onanyan <meri.onanyan@volo.global>
2026-08-31 18:27:39 +0400

    Updated homework

--- lesson_33_homework.py
+++ lesson_33_homework.py
@@ -2,29 +2,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+browsers = ["chrome", "firefox"]
 
 def test(driver):
-    # TODO, need to check here which broser, after open driver. Also add checking if unsuported browser, raise exception
-    driver.get("https://www.python.org/")
+    #FIXED, need to check here which broser, after open driver. Also add checking if unsuported browser, raise exception
+    
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        raise ValueError(f"Unsupported browser: {browser}")
+
     driver.maximize_window()
 
-    search = driver.find_element(By.ID, "id-search-field")
-    sent = driver.find_element(By.ID, "submit")
+    try:
+        driver.get("https://www.python.org/")
 
-    search.send_keys("BlaBla")
-    sent.click()
+        search = driver.find_element(By.ID, "id-search-field")
+        sent = driver.find_element(By.ID, "submit")
 
-    time.sleep(3)
+        search.send_keys("BlaBla")
+        sent.click()
 
-    information = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/form/ul')
+        time.sleep(3)
 
-    if information.text == "No results found.":
-        print("Everything is ok")
-    else:
-        print("Something went wrong")
+        information = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/form/ul')
+
+        if information.text == "No results found.":
+            print("Everything is ok")
+        else:
+            print("Something went wrong")
+
+    except Exception as error:
+        print(f"[{browser}] Test ERROR: {error}")
+
     
     driver.close()
 
-# TODO, add main block, and give list of browsers, based on for cycle pass driver to function, no need to repeat function call
-test(webdriver.Chrome())
-test(webdriver.Firefox())
+# FIXED, add main block, and give list of browsers, based on for cycle pass driver to function, no need to repeat function call
+
+if __name__ == "__main__":
+
+    for browser in browsers:
+        print(f"Running test on: {browser}")
+        test(browser)
